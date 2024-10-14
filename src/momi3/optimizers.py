fb0c50878a4741e7678e28013f84394b80d03543
enesdilber <ns108ns@gmail.com>
2024-10-13 19:55:52 -0700

    fix optimizer

--- src/momi3/optimizers.py
+++ src/momi3/optimizers.py
@@ -6,11 +6,10 @@ import cvxpy
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import numpy as np
+import optax
 from jaxopt import ProjectedGradient
 from sparse import COO
 from tqdm.autonotebook import tqdm, trange
-
-import optax
 
 from momi3.Params import Params
 
@@ -57,9 +56,9 @@ def ProjectedGradient_optimizer(
         theta_train_dict = {}
         for paths, value in zip(theta_train_keys, theta_train):
             theta_train_dict[paths] = float(value)
-        return negative_loglik_with_gradient(
-            params, jsfs, theta_train_dict, return_array=True
-        )
+        val, grad = negative_loglik_with_gradient(params, jsfs, theta_train_dict)
+        grad = jnp.array([grad[key] for key in theta_train_keys])
+        return val, grad
 
     pg = ProjectedGradient(
         fun=FwG,
@@ -87,6 +86,7 @@ def ProjectedGradient_optimizer(
         pg_state = pg.init_state(theta_train_0)
         loss = []
         theta_train_hat = theta_train_0.copy()
+        logliks = []
         for i in trange(maxiter):
             theta_train_hat, pg_state = pg.update(
                 theta_train_hat, pg_state, hyperparams_proj=(A, b, G, h)
@@ -103,7 +103,9 @@ def ProjectedGradient_optimizer(
                     show_boundry_message = False
 
             # Messages:
-            tqdm.write(f"loglik={-neg_log_lik:.5g}")
+            loglik = -neg_log_lik
+            logliks.append(loglik)
+            tqdm.write(f"loglik={loglik:.5g}")
             params_message = ", ".join(
                 [f"{i}={j:.2g}" for i, j in zip(theta_train_keys, theta_train_hat)]
             )
@@ -112,8 +114,8 @@ def ProjectedGradient_optimizer(
 
             loss.append(pg_state.error)
 
-        plt.plot(range(1, maxiter + 1), loss)
-        plt.ylabel("Error")  # TODO: Not sure what this error is
+        plt.plot(range(1, maxiter + 1), logliks)
+        plt.ylabel("Loglik")  # TODO: Not sure what this error is
         plt.xlabel("Iteration Number")
 
     else:
@@ -145,9 +147,16 @@ def optax_step(optimizer, f, theta_train_dict, opt_state):
 
 
 def optax_for_momi(
-    optimizer, momi, params, jsfs, niter, transformed=True, theta_train_dict=None, opt_state=None, history=None
+    optimizer,
+    momi,
+    params,
+    jsfs,
+    niter,
+    transformed=True,
+    theta_train_dict=None,
+    opt_state=None,
+    history=None,
 ):
-
     if theta_train_dict is None:
         theta_train_dict = params.theta_train_dict(transformed)
 
@@ -168,8 +177,10 @@ def optax_for_momi(
         return v, g
 
     for i in trange(niter):
-        history['ttds'].append(dict(zip(train_keys, theta_train)))
-        theta_train, opt_state, loss_value = optax_step(optimizer, f, theta_train, opt_state)
-        history['LLs'].append(loss_value)
+        history["ttds"].append(dict(zip(train_keys, theta_train)))
+        theta_train, opt_state, loss_value = optax_step(
+            optimizer, f, theta_train, opt_state
+        )
+        history["LLs"].append(loss_value)
 
     return dict(zip(train_keys, theta_train)), opt_state, history
