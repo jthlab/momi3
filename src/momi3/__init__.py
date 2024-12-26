a48d9c61e8a851ffc87a1d023f0cbb166cc08632
Jonathan Terhorst <terhorst@gmail.com>
2024-12-26 16:38:57 -0500

    cache tweak

--- src/momi3/__init__.py
+++ src/momi3/__init__.py
@@ -12,6 +12,8 @@ from .momi import Momi3  # noqa: F401
 __all__ = ["Momi3"]
 
 jax.config.update("jax_enable_x64", True)
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
 jax.config.update("jax_compilation_cache_dir", platformdirs.user_cache_dir("momi3"))
 logging.getLogger("jax").setLevel(logging.INFO)
 
